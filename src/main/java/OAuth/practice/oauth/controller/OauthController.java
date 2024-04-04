e6e7b9ca4b507efc0e1d7fe210a0b18204ac0539
bagjinhwan <spdlqjxp@gmail.com>
2024-04-04 19:52:22 +0900

    OauthController completed

--- src/main/java/OAuth/practice/oauth/controller/OauthController.java
+++ src/main/java/OAuth/practice/oauth/controller/OauthController.java
@@ -1,13 +1,22 @@
 package OAuth.practice.oauth.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Map;
+
+@RestController
+@RequestMapping("/oauth")
 public class OauthController {
+    @GetMapping("/loginfo")
+    public String getJson(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        // OAuth2User에서 사용자의 속성을 가져옵니다.
+        Map<String, Object> attributes = oAuth2User.getAttributes();
 
-    @GetMapping("/")
-    public String Oauth(){
-        return "/index.html";
+        // 속성을 문자열로 반환합니다.
+        return attributes.toString();
     }
 }
