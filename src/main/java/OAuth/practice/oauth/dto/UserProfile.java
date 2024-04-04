85b80bada3e9c7322ebad6af910df32938fa5101
bagjinhwan <spdlqjxp@gmail.com>
2024-04-04 19:52:33 +0900

    UserProfile updated

--- src/main/java/OAuth/practice/oauth/dto/UserProfile.java
+++ src/main/java/OAuth/practice/oauth/dto/UserProfile.java
@@ -1,4 +1,7 @@
+package OAuth.practice.oauth.dto;
+
 import OAuth.practice.oauth.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +11,7 @@ public class UserProfile {
     private String username; // 사용자 이름
     private String provider; // 로그인한 서비스
     private String email; // 사용자의 이메일
+
 
     public User toEntity() {
         return User.builder()
