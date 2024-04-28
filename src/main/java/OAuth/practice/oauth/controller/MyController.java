45e4e45e86ba6e14b08fd3d0dd5f5465b76c011d
bagjinhwan <spdlqjxp@gmail.com>
2024-04-28 20:01:42 +0900

    240427 add Main & Mypage Controller

---
+++ src/main/java/OAuth/practice/oauth/controller/MyController.java
@@ -0,0 +1,12 @@
+package OAuth.practice.oauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping("/my")
+    public String my() {
+        return "my";
+    }
+}
