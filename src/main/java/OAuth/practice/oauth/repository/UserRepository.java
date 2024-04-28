9896a7bfde123f9b403381dc21745a94ee938ee1
bagjinhwan <spdlqjxp@gmail.com>
2024-04-28 20:03:35 +0900

    240428 add method for signup and login in UserRepository

--- src/main/java/OAuth/practice/oauth/repository/UserRepository.java
+++ src/main/java/OAuth/practice/oauth/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package OAuth.practice.oauth.repository;
 
-import OAuth.practice.oauth.entity.User;
+import OAuth.practice.oauth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByEmailAndProvider(String email, String provider);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findUserByEmailAndProvider(String email, String provider);
+
+    Boolean existsByUserId(String userId);
+
+    Boolean existsByEmail(String email);
+
+    UserEntity findByUserId(String userId);
 }
